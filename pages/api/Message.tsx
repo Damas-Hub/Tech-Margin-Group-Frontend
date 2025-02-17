4dd05fafdadd84ccf9071fddc5273c25af26e351
Damas-Hub <selormeyh@gmail.com>
2025-02-17 20:55:31 +0000

    Add Admin, Store, and Message components with sidebar navigation

---
+++ pages/api/Message.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Message = () => {
+  return (
+    <div>
+      <h2>Messages</h2>
+      <p>This is the message section</p>
+    </div>
+  );
+};
+
+export default Message;
