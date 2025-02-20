39bd6fc85c6e1621af9ad722eb7bf65003199049
Damas-Hub <selormeyh@gmail.com>
2025-02-20 14:51:07 +0000

    Update StaffProfile

--- componnets/StaffProfile.tsx
+++ componnets/StaffProfile.tsx
@@ -1,36 +1,35 @@
-import React from 'react';
-import styles from './StaffProfile.module.css'; // Importing the CSS module
+import React from "react";
+import styles from "./StaffProfile.module.css";
 
-const StaffProfile: React.FC = () => {
+interface StaffProfileProps {
+  profilePic: string;
+  name: string;
+  staffId: string;
+  role: string;
+  contactNumber: string;
+}
+
+const StaffProfile: React.FC<StaffProfileProps> = ({
+  profilePic,
+  name,
+  staffId,
+  role,
+  contactNumber,
+}) => {
   return (
     <div className={styles.container}>
       <div className={styles.card}>
         <div className={styles.front}>
-          <div className={styles.cardTop}>
-            <p className={styles.cardTopPara}>Profile</p>
-          </div>
-          <svg xmlns="http://www.w3.org/2000/svg" width={100} height={100} fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
-            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-            <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-          </svg>
-          <p className={styles.heading}> Secretary </p>
-          <p className={styles.follow}>Damas Hub</p>
-        </div>
-        <div className={styles.back}>
-          <p className={styles.heading}>Follow Me</p>
-          <svg xmlns="http://www.w3.org/2000/svg" width={100} height={100} fill="currentColor" className="bi bi-person-add" viewBox="0 0 16 16">
-            <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z" />
-            <path d="M8.256 14a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z" />
-          </svg>
-          <div className={styles.icons}>
-            <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} fill="currentColor" className="bi bi-instagram" viewBox="0 0 16 16">
-              <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584s6.592 2.958 6.592 6.592c-.004 3.639-2.961 6.592-6.592 6.592z" />
-            </svg>
-          </div>
+          <div className={styles.cardTop}></div>
+          <img src={profilePic} alt="Profile" className={styles.profileImage} />
+          <p className={styles.heading}>{role}</p>
+          <p className={styles.follow}>{name}</p>
+          <p className={styles.follow}>Staff ID: {staffId}</p>
+          <p className={styles.follow}>Contact: {contactNumber}</p>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default StaffProfile;
--- pages/admin/Staffs.tsx
+++ pages/admin/Staffs.tsx
@@ -4,7 +4,13 @@ import React from 'react'
 const Staffs = () => {
   return (
     <div>Staffs
-<StaffProfile />
+  <StaffProfile
+      profilePic="https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg"
+      name="John Doe"
+      staffId="ST12345"
+      role="Secretary"
+      contactNumber="+1234567890"
+    />
     </div>
   )
 }
