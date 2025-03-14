9d6455ae42e0f7096bd4ab0f8ef0d7cf8f3b0d49
Damas-Hub <selormeyh@gmail.com>
2025-03-14 18:22:59 +0000

    Add logout confirmation modal and integrate toast notifications in AdminDashboard

--- pages/admin/Message.tsx
+++ pages/admin/Message.tsx
@@ -1,12 +1,14 @@
- import StaffAccountForm from '@/componnets/StaffAccountForm'
-import React from 'react'
- 
- const Message = () => {
-   return (
-     <div>
-      <StaffAccountForm onClose={() => { /* handle close */ }} />
-     </div>
-   )
- }
- 
- export default Message
+import StaffAccountForm from '@/componnets/StaffAccountForm';
+import React, { useState } from 'react';
+
+const Message = () => {
+  const [isFormOpen, setIsFormOpen] = useState(true);  
+
+  return (
+    <div>
+      {isFormOpen && <StaffAccountForm onClose={() => setIsFormOpen(false)} />}
+    </div>
+  );
+};
+
+export default Message;
